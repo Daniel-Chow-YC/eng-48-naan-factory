46c06f8c89b54b546857178cde9bfe947f1f48b5
Daniel Chow <DChow@spartaglobal.com>
2019-12-04 14:22:25 +0000

    wrote 2 tests for tdd

--- naan_factory.py
+++ naan_factory.py
@@ -1,4 +1,12 @@
 # Basis of a test
+# you'll be testing functions or methods, these need to be called or initialised
 # having controlled inputs for known outputs
     # and testing for these
 
+# Make test for make_dough
+# To make dough it will take in 'water' and 'flour' to make dough.
+print(make_dough('water', 'flour') == 'dough')
+
+# Make test for bake_dough
+#Then with the dough we should be able to put it into the oven, and get out a Naan.
+print(bake_dough('dough') == 'Naan')
